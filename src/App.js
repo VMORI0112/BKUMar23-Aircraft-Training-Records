34c60427c54c5452a46960de82250d84c57e0469
VMORI0112 <victorfernan_moriganador@hotmail.com>
2020-02-15 23:06:52 +0000

    update

--- src/App.js
+++ src/App.js
@@ -10,11 +10,14 @@ const Home = lazy(() => import('./js/views/Home/Home'));
 const LoggedHome = lazy(() => import('./js/views/LoggedHome/LoggedHome'));
 const Login = lazy(() => import('./js/views/Login/Login'));
 const Register = lazy(() => import('./js/views/Register/Register'));
+const CurrentRecords = lazy(() => import('./js/views/currentRecords/currentRecords'));
 
 function App() {
 
-  const [backen_url] = useState('https://iwash-backend.herokuapp.com/');
-  const [Auth, setAuth] = useState(null);
+    const [backen_url] = useState('https://iwash-backend.herokuapp.com/');
+    const [Auth, setAuth] = useState(null);
+    // const [backen_url_records] = useState(' https://3000-a8e8103b-38a9-441b-a8b2-d02d84f3caf2.ws-us02.gitpod.io/');
+    // const [trainData, setTrainData] = useState();
 
   let tokenAuth = localStorage.getItem('token');
   // let userId = localStorage.getItem('userID');
@@ -29,6 +32,13 @@ function App() {
     };
   },[])
 
+//    useEffect(() => {
+//     fetch(backen_url_records+'traindata')
+//       .then(res => res.json())
+//       .then(res => setTrainData(res))
+//       .catch(error => console.log('error: ', error) );
+//   },[backen_url_records])
+
   const providerValue = useMemo(() => ({ backen_url, Auth, setAuth, windowHeight }), [backen_url, Auth, setAuth, windowHeight]);
 
   return (
@@ -41,6 +51,7 @@ function App() {
                 {!tokenAuth ? <Route exact path="/" component={Home} /> : <Route exact path="/" component={LoggedHome}/> }
                 <Route path="/login" component={Login} />
                 <Route path="/register" component={Register} />
+                <Route path="/training-records" component={CurrentRecords} />
                 <Route render={() => <NotFound/>} />
               </Switch>
             </section>
---
+++ src/js/views/currentRecords/currentRecords.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import styles from './currentRecords.module.css';
+
+const CurrentRecords = () => {
+
+    // const {trainData} = useContext(UserContext);
+    const [myValue, setMyValue] = useState('');
+
+    const [newRecords, setNewRecords] = useState();
+
+    const searchMyRecords = () => {
+        console.log("dwwe", myValue)
+
+        fetch('https://3000-a8e8103b-38a9-441b-a8b2-d02d84f3caf2.ws-us02.gitpod.io/traindata', {
+            method: 'POST',
+            body:JSON.stringify({
+                    "employerId": myValue
+                }),
+            cors: 'no-cors',
+            headers:{
+                'Content-Type': 'application/json'
+            }
+            }).then(res => res.json())
+            .then(response => {
+                setNewRecords(response)
+            })
+            .catch(error => {
+                alert("Something Went Wrong!", error)
+            });
+    }
+
+    console.log(newRecords)
+
+    return (
+        <div className={styles.main}>  
+
+            <label>Insert Your Employer ID</label>
+            <input type="text" placeholder="Ex: 123343" onChange={(e) => setMyValue(e.target.value)} />
+            <button onClick={searchMyRecords}>Search my recrods</button>
+
+           
+            <table class="table">
+                <thead>
+                    <tr>
+                    <th scope="col">name</th>
+                    <th scope="col">employer ID</th>
+                    <th scope="col">course number</th>
+                    <th scope="col">Training Groupe</th>
+                    <th scope="col">STA</th>
+                    <th scope="col">DateAtten</th>
+                    <th scope="col">ANP</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {!newRecords ? "loading..." : newRecords.map((items,index) => {
+                        return (
+                                <tr key={index}>
+                                    <td>{items.name}</td>
+                                    <td>{items.employerId}</td>
+                                    <td>{items.courseNumber}</td>
+                                    <td>{items.trainingGroup}</td>
+                                    <td>{items.sta}</td>
+                                    <td>{items.dateAtten}</td>
+                                    <td>{items.anp}</td>
+                                </tr>
+                            
+                        
+                        )
+                    }).sort()}
+         </tbody>
+                </table>
+        </div>
+    );
+};
+
+export default CurrentRecords;
